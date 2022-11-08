a1da6d40881878e6bed45629cfb61b2abe8299ee
David Wolf <68775926+devidw@users.noreply.github.com>
2022-11-08 22:06:23 +0000

    update 1 file and delete 1 file

--- new.sh
+++ new.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# if first arg not set exit
+if [ -z "$1" ]; then
+    echo "Usage: $0 <name>"
+    exit 1
+fi
+
 domain=$1
 folderpath="./sites/$domain/"
-filepath=$folderpath"index.styl"
+filepath=$folderpath"index.user.styl"
+
+echo $filepath
 
 mkdir $folderpath
 cp -r ./templates/* $folderpath
-sed -i "s/{domain}/$domain/g" $filepath
-code $filepath 
+
+# replace DOMAIN with $domain
+sed -i "s/DOMAIN/$domain/g" $filepath
+
+code $filepath
