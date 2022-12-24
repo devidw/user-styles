08b8b55ad46b2a9214a6c9f08003f029ac50b03f
David Wolf <68775926+devidw@users.noreply.github.com>
2022-12-24 22:17:37 +0100

    update quora.user.js

--- scripts/quora.user.js
+++ scripts/quora.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Quora answers only, no related
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Filter for answers by default, not related questions
 // @author       devidw
 // @match        https://www.quora.com/*
+// @updateURL    https://raw.githubusercontent.com/devidw/user-styles/master/scripts/quora.user.js
 // ==/UserScript==
 
-; (function () {
+; (async function () {
   "use strict"
 
   function getElementByXPath(xpath) {
@@ -21,15 +22,26 @@
   }
 
   // Get button with text containing "All related"
-  const button = getElementByXPath(
+  const getButton = () => getElementByXPath(
     '//*[@id="mainContent"]/div[3]/div/div[1]/div/div/button'
   )
+
+  let button
+  let tries = 0
+
+  while (!button && tries < 10) {
+    await new Promise((resolve) => setTimeout(resolve, 100))
+    button = getButton()
+    tries++
+  }
 
   button.click()
 
   const option = getElementByXPath(
     '//*[@id="mainContent"]/div[3]/div/div[1]/div/div[2]/div/div[1]/div/div/div[2]'
   )
+
+  if (!option) return console.warn("Couldn't find option element")
 
   option.click()
 })()
